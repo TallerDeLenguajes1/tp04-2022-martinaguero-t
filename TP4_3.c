8f69e7396500485d511171a63e7558cc6c4de432
Martin Agüero Trevisan <aguerotrevisanmartin@gmail.com>
2022-04-30 11:43:37 -0300

    Se crearon nuevas funciones para modularizar el código y se modifica la función que carga las listas de tareas realizadas y pendientes!

--- TP4_3.c
+++ TP4_3.c
@@ -4,9 +4,11 @@
 
 #define BUFFSIZE 50
 
+// Tipificación
+
 struct Tarea
 {
-    int TareaID;
+    short TareaID;
     char *descripcion;
     int duracion;
 } typedef Tarea;
@@ -18,15 +20,31 @@ struct Nodo{
 
 typedef Nodo* Lista;
 
+// -------------
+
+
+// Funciones
+
 void cargarTareas(Lista* listaTareas, short cantTareas);
-Lista cargarTarea(Lista listaTareas, short ID);
+void cargarTareaAlFinal(Lista *listaTareas, short ID);
+
+Nodo* crearNodo(short ID);
+
+void enlazarNodoAlFinal(Lista* listaTareas, Nodo* nuevoNodo);
+
 void mostrarTareas(Lista listaTareas);
 void mostrarTarea(Tarea tarea);
+
 void consultarEstadoTareas(Lista* listaTareasPendientes, Lista* listaTareasRealizadas);
+void consultarEstadoTareas2(Lista* listaTareasPendientes, Lista* listaTareasRealizadas);
+// La nueva función consultarEstadoTareas2 está mejor modularizada y carga las tareas a cada lista en orden respectivo (no invertido).
+
 Nodo* buscarPorID(Lista listaTareas, int ID);
 Nodo* buscarPorPalabra(Lista listaTareas, char* palabra);
+
 Lista liberarMemoria(Lista tareas);
 
+// ------------
 int main(){
 
     Lista tareasPendientes = NULL;
@@ -46,11 +64,12 @@ int main(){
 
     printf("=======================================\n");
 
-
     cargarTareas(&tareasPendientes,cantTareas);
 
     printf("A continuacion se presenta\ninformacion de las tareas cargadas:\n");
     mostrarTareas(tareasPendientes);
+    // Tras cargar las tareas, se las muestra
+
 
     printf("=======================================\n");
 
@@ -72,18 +91,25 @@ int main(){
     }
 
     printf("=======================================\n");
+    // Prueba de las funciones de búsqueda.
 
-
-    consultarEstadoTareas(&tareasPendientes,&tareasRealizadas);
+    consultarEstadoTareas2(&tareasPendientes,&tareasRealizadas);
+    // Consultamos al usuario si cada tarea fue realizada o no.
 
     printf("A continuacion se presenta\ninformacion de las tareas pendientes:\n");
     mostrarTareas(tareasPendientes);
     printf("A continuacion se presenta\ninformacion de las tareas realizadas:\n");
     mostrarTareas(tareasRealizadas);
 
+
     tareasRealizadas = liberarMemoria(tareasRealizadas);
     tareasPendientes = liberarMemoria(tareasPendientes);
-    
+    // Liberación de memoria
+
+    mostrarTareas(tareasRealizadas);
+    mostrarTareas(tareasPendientes);
+    // Control para verificar que las listas quedaron vacías.
+
     return 0;
 
 }
@@ -103,21 +129,25 @@ void cargarTareas(Lista* listaTareas, short cantTareas){
 
     for (short i = 0; i < cantTareas; i++)
     {
-        *listaTareas = cargarTarea(*listaTareas,i+1);
+        cargarTareaAlFinal(listaTareas,i+1);
         printf("=======================================\n");
 
     }
     
 } 
 
-Lista cargarTarea(Lista listaTareas, short ID){
+Nodo* crearNodo(short ID){
 
-    short duracionTarea;
+    int duracionTarea;
+
+    Nodo* nuevoNodo = (Nodo*) malloc(sizeof(Nodo));
+
+    printf("Ingrese la descripcion de la tarea %d: \n",ID);
+
     char* buffer = (char*) malloc(BUFFSIZE);
-
-    printf("Ingrese la descripcion de la tarea %d:\n",ID);
+    // Buffer para pedir la descripción de la tarea
     gets(buffer);
-    
+
     do
     {
         printf("Ingrese la duracion de la tarea, entre 10 y 100: \n");
@@ -126,62 +156,66 @@ Lista cargarTarea(Lista listaTareas, short ID){
 
     } while (duracionTarea < 10 || duracionTarea > 100);
 
+    nuevoNodo->sigNodo = NULL;
+    // Inicializo por defecto el puntero del nuevo nodo en NULL
 
-    if(listaTareas != NULL){
+    (nuevoNodo->tarea).descripcion = (char*) malloc(strlen(buffer)+1);
+    strcpy((nuevoNodo->tarea).descripcion,buffer);
+
+    (nuevoNodo->tarea).duracion = duracionTarea;
+    (nuevoNodo->tarea).TareaID = ID;
+
+    free(buffer);
+
+    return nuevoNodo;
+
+}
+
+void enlazarNodoAlFinal(Lista* listaTareas, Nodo* nuevoNodo){
+    if(*listaTareas != NULL){
 
         // Si la lista no esta vacia,
-        Lista headAux = listaTareas;
+        Lista headAux = *listaTareas;
         // uso un head auxiliar para llegar al último nodo de la lista y no mover el head de la lista
 
         while(headAux->sigNodo != NULL){
             headAux = headAux->sigNodo;
         }
 
-        headAux->sigNodo = (Lista) malloc(sizeof(Nodo));
-        // Reservo memoria para un nuevo nodo que se conectará al último de la lista
-
-        headAux->sigNodo->tarea.duracion = duracionTarea;
-        headAux->sigNodo->tarea.TareaID = ID;
-        headAux->sigNodo->tarea.descripcion = (char*)malloc(strlen(buffer)+1);
-        strcpy(headAux->sigNodo->tarea.descripcion,buffer);
-        // Guardo datos
-
-        headAux->sigNodo->sigNodo = NULL;
-        // El nuevo nodo creado apuntará a NULL.
-
-        return listaTareas;
-        // Esto no es necesario, pero la función retorna Lista.
-    
+        headAux->sigNodo = nuevoNodo;
+        // Ahora el último nodo apunta a el nuevo nodo creado, que ya está inicializado con su puntero en NULL.
 
     } else {
         // Si la lista esta vacia, cargo el nuevo nodo directamente.
 
-        listaTareas = (Lista) malloc(sizeof(Nodo));
+        *listaTareas = nuevoNodo;
+        // Notar que el nuevo nodo ya viene inicializado con su puntero en NULL, por lo que no hago la asignación
+        // nuevoNodo = *listaTareas
 
-        listaTareas->tarea.TareaID = ID;
-        listaTareas->tarea.duracion = duracionTarea;
-        listaTareas->tarea.descripcion = (char*)malloc(strlen(buffer)+1);
-        strcpy(listaTareas->tarea.descripcion,buffer);
-        // Se cargan los datos
-
-        listaTareas->sigNodo = NULL;
-
-        return listaTareas;
-
+        // Además, en este caso la lista está vacía, no hace falta tener en cuenta el enlazamiento de otros nodos.
     }
+}
 
-    free(buffer);
+void cargarTareaAlFinal(Lista* listaTareas, short ID){
+
+    Nodo* nuevoNodo = crearNodo(ID);
+
+    enlazarNodoAlFinal(listaTareas,nuevoNodo);
 
 }
 
 void mostrarTareas(Lista listaTareas){
 
-    while(listaTareas != NULL){
+    if (listaTareas == NULL){
+        printf("La lista no tiene tareas cargadas! \n");
+    } else {
 
-        mostrarTarea(listaTareas->tarea);
-        listaTareas = listaTareas->sigNodo;
-
+        while(listaTareas != NULL){
+            mostrarTarea(listaTareas->tarea);
+            listaTareas = listaTareas->sigNodo;
+        }
     }
+    
 }
 
 void mostrarTarea(Tarea tarea){
@@ -236,6 +270,48 @@ void consultarEstadoTareas(Lista* listaTareasPendientes, Lista* listaTareasReali
 
 }
 
+Nodo* quitarNodo(Lista* listaTareas){
+
+    Nodo* aux = NULL;
+
+    if(*listaTareas != NULL){
+        aux = *listaTareas;
+        *listaTareas = (*listaTareas)->sigNodo;
+        aux->sigNodo = NULL;
+        // Para poder agregar el nodo a otra lista de ser necesario, modifico su puntero a NULL
+    }
+
+    return aux;
+}
+
+void consultarEstadoTareas2(Lista* listaTareasPendientes, Lista* listaTareasRealizadas){
+
+    Lista listaTareasPendientesAux = NULL;
+    short realizada;
+
+    printf("Ahora analicemos cada tarea: \n");
+
+    while(*listaTareasPendientes != NULL){
+
+        mostrarTarea((*listaTareasPendientes)->tarea);
+
+        do
+        {
+            printf("Fue realizada? 1: si, 0; no\n");
+            scanf("%hd", &realizada);
+
+        } while (realizada != 0 && realizada != 1);
+
+        if(realizada == 1){
+            enlazarNodoAlFinal(listaTareasRealizadas,quitarNodo(listaTareasPendientes));
+        } else {
+            enlazarNodoAlFinal(&listaTareasPendientesAux,quitarNodo(listaTareasPendientes));
+        }
+    }
+
+    *listaTareasPendientes = listaTareasPendientesAux;
+
+}
 
 Nodo* buscarPorID(Lista listaTareas, int ID){
 
